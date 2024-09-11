67fa01d27bcead44d7cf64c51b6dbe948e6773f8
365cent <74791570+365cent@users.noreply.github.com>
2024-09-10 23:54:16 -0400

    feat: Add @cloudflare/next-on-pages dependency and configuration
    This commit adds the "@cloudflare/next-on-pages" dependency to the project's package.json file and configures it in the next.config.js file. This dependency is required for adapting the project for Cloudflare Pages. The reactStrictMode option is also set to true in the next.config.js file.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+const { withNextOnPages } = require('@cloudflare/next-on-pages/plugin');
+
+module.exports = withNextOnPages(nextConfig);
